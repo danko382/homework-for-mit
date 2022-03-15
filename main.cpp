5917ff5584277a96ed4ea8c34f38f8023792f2d1
danko328 <daniil-kondrashov-rabotaet@yandex.ru>
2022-03-15 18:31:15 +0400

    добавил функцию fib, вычисляющую n-ое число фибоначчи

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+int fib(int value){
+    if (0 == value || 1 == value) return 1;     
+    else return fib(value - 1) + fib(value - 2);		
+}
 int main(){
 
     std::cout<< "Hello World!" << std::endl;
+    
+    int n;
+    std::cin >> n;
+    std::cout << fib(n-1) << std::endl;
 
     return 0;
 }
